044f8e69f8c234ea698a446dea7af6197228d581
Anton Popov <ademaro@ya.ru>
2022-12-21 10:40:30 +0530

    Add currentLocale variable

--- src/pristine.js
+++ src/pristine.js
@@ -107,7 +107,7 @@ export default function Pristine(form, config, live) {
             let messageMatch = name.match(MESSAGE_REGEX);
             if (messageMatch !== null) {
               let locale =
-                messageMatch[1] === undefined ? 'en' : messageMatch[1];
+                messageMatch[1] === undefined ? currentLocale : messageMatch[1];
               if (!messages.hasOwnProperty(locale)) messages[locale] = {};
               messages[locale][
                 name.slice(0, name.length - messageMatch[0].length)
